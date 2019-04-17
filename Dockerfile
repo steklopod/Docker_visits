0362d276d34593b8a2f704a9c9f93f22bcd0fe39
Steklopod <steklopod@gmail.com>
2019-04-17 17:53:46 +0300

    Init another example

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-ARG DEPENDENCY=target/dependency
-COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
-COPY ${DEPENDENCY}/META-INF /app/META-INF
-COPY ${DEPENDENCY}/BOOT-INF/classes /app
-ENTRYPOINT ["java","-cp","app:app/lib/*","hello.Application"]
+FROM openjdk:10-jre-slim
+COPY ./target/hola-docker-1.0.0-SNAPSHOT.jar /usr/src/hello/
+WORKDIR /usr/src/hello
+EXPOSE 8080
+CMD ["java", "-jar", "hello-docker-1.0.0-SNAPSHOT.jar"]
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,20 +2,22 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
+@SpringBootApplication
 public class Application {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
+    @GetMapping
+    public String home() throws UnknownHostException {
+        return "Привет!  " + InetAddress.getLocalHost().getHostAddress();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
