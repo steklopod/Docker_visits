a95781bd72b7ac3df04d383e3a5ff1ee026a76de
Steklopod <steklopod@gmail.com>
2019-04-18 13:24:17 +0300

    Visit counting in main page.

--- src/main/java/steklopod/Application.java
+++ src/main/java/steklopod/Application.java
@@ -1,5 +1,6 @@
 package steklopod;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @SpringBootApplication
 public class Application {
     private static final Logger log = LoggerFactory.getLogger("application");
+    private static final Map<String, AtomicInteger> visits = new HashMap<>();
+
 
     public static void main(String[] args) {
-        log.info("Зайди на: http://192.168.99.100:8080");
+        log.info("Зайди на: http://192.168.99.100:8080 если запуск производиться из DOCKER");
+        log.info("или   на: http://localhost:8080      если запускаешь локально");
         SpringApplication.run(Application.class, args);
     }
 
@@ -24,8 +31,14 @@ public class Application {
     @GetMapping
     public String home() throws UnknownHostException {
         String hostAddress = InetAddress.getLocalHost().getHostAddress();
-        log.info("hostAddress: " + hostAddress);
-        return "Привет! Твой хост адресс: " + hostAddress;
+        return "Привет! Твой хост адресс: " + hostAddress +
+                "\n >>> Колличество визитов: [ " + getCountOfVisits(hostAddress) + " ] <<<";
+    }
+
+    @NotNull
+    private static synchronized Integer getCountOfVisits(String hostAddress) {
+        visits.putIfAbsent(hostAddress, new AtomicInteger(0));
+        return visits.get(hostAddress).incrementAndGet();
     }
 
 
